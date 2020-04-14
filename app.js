e3463220e5852e378e96fef4ea3a1c94124c201f
Connor Dillon <connor@codewithconnor.com>
2020-04-14 00:36:09 -0700

    Built out backend side of edit state handling

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ import { ItemCtrl } from './item.js'
 import { UICtrl } from './ui.js'
 import { StorageCtrl } from './storage.js'
 
+const itemCtrl = ItemCtrl()
+const uiCtrl = UICtrl(itemCtrl)
+const storageCtrl = StorageCtrl()
+
 //& App Controller
 const App = (function (ItemCtrl, UICtrl, StorageCtrl) {
-  // Test: ensure JS modules are returning outputs to App.js correctly
-  //   console.log('ItemCtrl.init: ', ItemCtrl.init())
-  //   console.log('UICtrl.init: ', UICtrl.init())
-  //   console.log('StorageCtrl.init: ', StorageCtrl.init())
+  //& Test: ensure JS modules are returning outputs to App.js correctly
+  //   console.log('Test.App.ItemCtrl.init: ', ItemCtrl.init())
+  //   console.log('Test.App.UICtrl.init: ', UICtrl.init())
+  //   console.log('Test.App.StorageCtrl.init: ', StorageCtrl.init())
+  //& Test: Use window.test in Chrome console to see if ItemCtrl and UICtrl are being fed to the DOM correctly
   //   window.test = { ItemCtrl, UICtrl }
+  //   window.test = ItemCtrl.getCurrentItem()
 
   // Load Event Listeners
   const loadEventListerners = () => {
@@ -16,22 +22,30 @@ const App = (function (ItemCtrl, UICtrl, StorageCtrl) {
     const UISelectors = UICtrl.getSelectors()
     // Add Item Event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
+
+    // Edit icon click event
+    // Need to use event delegation to find the edit icons because they will be created after the DOM loads, so we'll be looking for a pre-existing parent element (the list that its in) that holds our clickable edit icon
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit)
   }
 
   // Add Item Submit
   const itemAddSubmit = (e) => {
-    // Test: Make sure "Add Meal" button is triggering properl
-    // console.log('Add')
+    //& Test: Make sure "Add Meal" button is triggering properl
+    // console.log('Test.App.itemAddSubmit: Add meal button click event triggered')
     const input = UICtrl.getItemInput()
 
-    // Test: Make sure "Meal" and "Calories" input fields are being stored correctly when "Add Meal" button is clicked
-    // console.log(input)
+    //& Test: Make sure "Meal" and "Calories" input fields are being stored correctly when "Add Meal" button is clicked
+    // console.log('Test.App.itemAddSubmit: Add Meal button correctly stored form submission', input)
 
     if (input.name !== '' && input.calories !== '') {
-      console.log('Test passed: Meal name and calories inputs are not null-ish')
+      //& Test: Checks if input fields are not empty
+      //   console.log('Test passed: Meal name and calories inputs are not null-ish')
+
       // Add item
       const newItem = ItemCtrl.addItem(input.name, input.calories)
-      console.log('ItemCtrl.logData(): ', ItemCtrl.logData())
+
+      //& Test: ensure item data is being logged correctly to item controller
+      //   console.log('Test.App.itemAddSubmit: ItemCtrl.logData(): ', ItemCtrl.logData())
 
       // Add item to UI list
       UICtrl.addListItem(newItem)
@@ -45,21 +59,63 @@ const App = (function (ItemCtrl, UICtrl, StorageCtrl) {
       // Clear fields after form submit
       UICtrl.clearInput()
     } else {
-      console.log('Test failed: Meal name and calories are null-ish')
+      //& Test: Checks if input fields are not empty
+      //   console.log('Test App.itemAddSubmit: Test failed: Meal name and calories are null-ish')
     }
 
+    e.preventDefault()
+  }
+
+  // Update Item Submit
+  const itemUpdateSubmit = (e) => {
+    //& Test: ensure list items are registering click events
+    // console.log('Test App.itemUpdateSubmit: test passed')
+
+    if (e.target.classList.contains('edit-item')) {
+      //& Test: ensure edit icons are registering click events
+      //   console.log('Test.App.itemUpdateSubmit: edit item clicked')
+      const listId = e.target.parentNode.parentNode.id
+      //& Test: ensure we're grabbing the correct list ID for each item in list
+      //   console.log('Test.App.itemUpdateSubmit, listId: ', listId)
+
+      // Break item-IDs into array so that we separate the ID number from the ID tag in the html
+      // Split it at the hyphen, because each ID tag looks like "item-#"
+      const listIdArr = listId.split('-')
+
+      //& Test: Ensure we're breaking the list ID tags correctly:
+      //   console.log('Test.App.itemUpdateSubmit, listIdArr: ', listIdArr)
+
+      // Get the actual id from the list ID array by grabbing the index @1
+      const id = parseInt(listIdArr[1])
+
+      // Get item
+      const itemToEdit = ItemCtrl.getItemById(id)
+
+      //& Test: ensure we're getting all of the correct data when an edit icon is clicked on
+      //   console.log('Test.App.itemUpdateSubmit, itemToEdit: ', itemToEdit)
+
+      // Set current item
+      ItemCtrl.setCurrentItem(itemToEdit)
+
+      // Add item to form
+      UICtrl.addItemToForm()
+    }
     e.preventDefault()
   }
 
   // Returning public methods
   return {
     init: function () {
-      // Test: ensure app is being initialized properly
-      //   console.log('Initializing App...')
+      // Clear edit state / set initial state
+      UICtrl.clearEditState()
+
+      //& Test: ensure app is being initialized properly
+      //   console.log('Test.App.init: Initializing App...')
 
       // Fetch items from data structure
       const items = ItemCtrl.getItems()
-      //   console.log(items)
+      //& Test: logs the current items stored in local storage, or items added via dev environment
+      //   console.log('Test.App.init: getItems(): ', items)
 
       // Check if any items already exist
       if (items.length === 0) {
@@ -79,7 +135,7 @@ const App = (function (ItemCtrl, UICtrl, StorageCtrl) {
       loadEventListerners()
     },
   }
-})(ItemCtrl(), UICtrl(), StorageCtrl())
+})(itemCtrl, uiCtrl, storageCtrl)
 
 // Initialize App
 App.init()
--- ui.js
+++ ui.js
@@ -1,9 +1,19 @@
+import { ItemCtrl } from './item.js'
+
+// let ItemControl = ItemCtrl()
+
 //& UI Controller
-export const UICtrl = (/*UICtrl*/) => {
+export const UICtrl = function (ItemCtrl) {
+  //& Test: ensure JS modules are returning outputs to App.js correctly
+  // console.log('Test.UI.ItemCtrl.init: ItemCtrl fed to UICtrl correctly?', ItemCtrl().init())
+
   // UI Selectors: for managing where selector functions are pointed
   const UISelectors = {
     itemList: '#item-list',
     addBtn: '.add-btn',
+    updateBtn: '.update-btn',
+    deleteBtn: '.delete-btn',
+    backBtn: '.back-btn',
     itemNameInput: '#item-name',
     itemCaloriesInput: '#item-calories',
     totalCalories: '.total-calories',
@@ -58,17 +68,61 @@ export const UICtrl = (/*UICtrl*/) => {
     },
 
     // Clear form submission fields
-    clearInput: function () {
+    clearInput: () => {
       document.querySelector(UISelectors.itemNameInput).value = ''
       document.querySelector(UISelectors.itemCaloriesInput).value = ''
+    },
+    addItemToForm: () => {
+      let currItem = () => {
+        // console.log('UI TEST 1: LogData: ', ItemCtrl.logData())
+        // console.log('UI TEST 2: getCurrentItem: ', ItemCtrl.getCurrentItem())
+        // let foo = ItemCtrl.logData().currentItem
+        // console.log('UI TEST 3: ItemCtrl.logData.currentItem: ', foo)
+        // console.log('UI TEST 3.5: window.test: ', window.test)
+
+        // console.log('UI TEST 4: PATH TO CURRENTITEM? ', ItemCtrl.logData().currentItem)
+
+        // sets currItem equal to the return of the enclosed arrow function: ItemCtrl.getCurrentItem
+        return ItemCtrl.getCurrentItem()
+      }
+
+      //& Test: Checks what currItem has been set to
+      // console.log('Test.UI.addItemToForm.currItem(), Is Current Item Set? ', currItem())
+
+      document.querySelector(UISelectors.itemNameInput).value = ItemCtrl.getCurrentItem().name
+      console.log('getCurrentItem().calories', ItemCtrl.getCurrentItem().name)
+
+      document.querySelector(UISelectors.itemCaloriesInput).value = ItemCtrl.getCurrentItem().calories
+      console.log('getCurrentItem().calories', ItemCtrl.getCurrentItem().calories)
     },
 
     // Hide Item List if List is Empty (to remove floating line at bottom of new UI instance)
     hideList: () => {
       document.querySelector(UISelectors.itemList).style.display = 'none'
     },
+
+    // Updates total calorie count
     showTotalCalories: (totalCalories) => {
       document.querySelector(UISelectors.totalCalories).textContent = totalCalories
+    },
+
+    // Clear Edit State
+    clearEditState: () => {
+      UICtrl().clearInput
+      //   document.querySelector(UISelectors.itemNameInput).value = ''
+      //   document.querySelector(UISelectors.itemCaloriesInput).value = ''
+      document.querySelector(UISelectors.updateBtn).style.display = 'none'
+      document.querySelector(UISelectors.deleteBtn).style.display = 'none'
+      document.querySelector(UISelectors.backBtn).style.display = 'none'
+      document.querySelector(UISelectors.addBtn).style.display = 'inline'
+    },
+
+    // Show Edit State
+    showEditState: () => {
+      document.querySelector(UISelectors.updateBtn).style.display = 'inline'
+      document.querySelector(UISelectors.deleteBtn).style.display = 'inline'
+      document.querySelector(UISelectors.backBtn).style.display = 'inline'
+      document.querySelector(UISelectors.addBtn).style.display = 'none'
     },
 
     getSelectors: () => {
